f94be92356462deb4b5320d602e97024376445cf
hoonboon <hongwen77@gmail.com>
2018-09-13 12:36:21 +0800

    Add meta tags for listing page

--- src/controllers/job.ts
+++ src/controllers/job.ts
@@ -85,7 +85,7 @@ export let getJobs = (req: Request, res: Response, next: NextFunction) => {
             const baseUrl = process.env.PUBLIC_SITE || "";
             const imgUrl = baseUrl + "/images/fbProfilePhoto2.jpg";
             const metaFb = generateMetaFacebook({
-                url: "/jobs",
+                url: "jobs",
                 type: "article",
                 title: ogTitle,
                 description: "Nok cari kijo kat Kelate? Orghe Kelate nok cari kijo kat luar? Acu tra cari kat sini.",
